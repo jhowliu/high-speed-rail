8d7c24f5fa5d8c7b7b04fa574de53d875b02c330
Jhow <jhowliu@gmail.com>
2017-11-14 11:53:48 +0800

    add router

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const requireDir = require('require-dir');
+const cors = require('cors');
 
 const app = express();
 
+const manifest = requireDir('./manifest');
+
 app.set('port', 3456);
+
+// allow cross-origin-resource-sharing
+app.use(cors());
 
 app.use(bodyParser.json({ type: 'application/json' }));
 app.use(bodyParser.urlencoded({ extended: false }));
 
+const THSR = require('./routes/THSR');
+
+app.use('/THSR', THSR);
 
 app.get('/', (req, res) => {
-  res.sendStatus(200);
+    res.sendStatus(200);
 });
 
 app.listen(app.get('port'), () => {
